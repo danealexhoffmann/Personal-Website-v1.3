d295d3daa91092161ae2d06555293f050f0cd66c
Dane Hoffmann <dane.hoffmann@pococode.co>
2022-05-20 11:58:33 +0200

    fixing responsive layout on different screen sizes

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -6,11 +6,13 @@ function Home() {
   return (
     <div className="home-container">
       <div className="home-content">
-        <h1 className="home-title">
-          Dane Alex
-          <br />
-          Hoffmann.
-        </h1>
+        <div className="home-title-container">
+          <h1 className="home-title">
+            Dane Alex
+            <br />
+            Hoffmann.
+          </h1>
+        </div>
         <h2 className="home-subtext">software developer & visual designer</h2>
         <SocialLinks />
       </div>
