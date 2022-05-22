c2a926031630bb53876b92f184a644d10ea926bd
Dane Hoffmann <dane.hoffmann@pococode.co>
2022-05-22 15:05:36 +0200

    add 'skills and tools' section and link to 'Projects'  to 'About' page

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,4 +1,15 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import reactNative from "../icons/react-native.svg";
+import redux from "../icons/redux.svg";
+import javascript from "../icons/javascript.svg";
+import nodejs from "../icons/nodejs.svg";
+import git from "../icons/git.svg";
+import html5 from "../icons/html-5.svg";
+import css3 from "../icons/css3.svg";
+import vscode from "../icons/visual-studio-code-2019.svg";
+import adobecc from "../icons/adobe-creative-cloud.svg";
+import figma from "../icons/figma.svg";
 import "./About.css";
 
 const About = () => {
@@ -26,6 +37,80 @@ const About = () => {
         >
           See My Resume!
         </a>
+      </div>
+      <div className="skills-div">
+        <h3 className="skills-title">skills and tools:</h3>
+        <div className="skills-items">
+          <ul className="skills-list-one">
+            <li className="skills-item">
+              <figure>
+                <img className="skill-icon" src={reactNative} alt="reactLogo" />
+                <figcaption>React</figcaption>
+              </figure>
+            </li>
+            <li className="skills-item">
+              <figure>
+                <img className="skill-icon" src={redux} alt="redux" />
+                <figcaption>Redux</figcaption>
+              </figure>
+            </li>
+            <li className="skills-item">
+              <figure>
+                <img className="skill-icon" src={javascript} alt="javascript" />
+                <figcaption>Javascript</figcaption>
+              </figure>
+            </li>
+            <li className="skills-item">
+              <figure>
+                <img className="skill-icon" src={nodejs} alt="nodejs" />
+                <figcaption>Nodejs</figcaption>
+              </figure>
+            </li>
+            <li className="skills-item">
+              <figure>
+                <img className="skill-icon" src={git} alt="git" />
+                <figcaption>Git</figcaption>
+              </figure>
+            </li>
+          </ul>
+          <ul className="skills-list-two">
+            <li className="skills-item">
+              <figure>
+                <img className="skill-icon" src={html5} alt="html5" />
+                <figcaption>HTML 5</figcaption>
+              </figure>
+            </li>
+            <li className="skills-item">
+              <figure>
+                <img className="skill-icon" src={css3} alt="css3" />
+                <figcaption>CSS 3</figcaption>
+              </figure>
+            </li>
+            <li className="skills-item">
+              <figure>
+                <img className="skill-icon" src={vscode} alt="vscode" />
+                <figcaption>VS Code</figcaption>
+              </figure>
+            </li>
+            <li className="skills-item">
+              <figure>
+                <img className="skill-icon" src={figma} alt="figma" />
+                <figcaption>Figma</figcaption>
+              </figure>
+            </li>
+            <li className="skills-item">
+              <figure>
+                <img className="skill-icon" src={adobecc} alt="adobecc" />
+                <figcaption>Adobe CC</figcaption>
+              </figure>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="projects-btn-div">
+        <NavLink className="projects-link" exact to="/projects">
+          <p className="projects-page">See My Projects</p>
+        </NavLink>
       </div>
     </div>
   );
